8b5dc09f4528d6f8e67db73045ed65f46487d28c
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-08-30 20:37:08 +0100

    OpenApi; First Commit: Config

---
+++ openapi/config/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = require('nconf')
+    .argv()
+    .env()
+    .file({ file: './default.json' })
+    .defaults({
+        port: process.env.RANDOM_API_PORT || 42002,
+        version: require('../package.json').version,
+        'redis.namespace': 'openapi',
+        'datasmart.url': 'http://localhost:4420',
+        'baseurlpath': '/api/v1',
+        'source.user': 'http://api.randomuser.me/',
+        'source.face': 'http://uifaces.com/api/v1/random'
+    });
